ebd4415ba446d158cfccb0c965e474622e95032b
aldocenga <eraldocenga@nmc.al>
2023-11-01 16:18:03 +0100

    Create route and controller method for adminlogout

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -294,20 +294,13 @@
                     <div class="dropdown-divider"></div>
 
                     <!-- item-->
-                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                    <a href="{{ route('admin.logout')}}" class="dropdown-item notify-item">
                         <i class="fe-log-out"></i>
                         <span>Logout</span>
                     </a>
 
                 </div>
             </li>
-
-            <li class="dropdown notification-list">
-                <a href="javascript:void(0);" class="nav-link right-bar-toggle waves-effect waves-light">
-                    <i class="fe-settings noti-icon"></i>
-                </a>
-            </li>
-
         </ul>
 
         <!-- LOGO -->
--- routes/web.php
+++ routes/web.php
@@ -36,4 +36,6 @@ Route::middleware(['auth','role:admin'])->group(function(){
 
 Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
 
+Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+
 });
