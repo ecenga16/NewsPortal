81c5df5db1ea1318763411ddaaa1550e150fd9ee
aldocenga <eraldocenga@nmc.al>
2023-11-17 13:50:29 +0100

    Minor fix at categories

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -64,27 +64,25 @@ $cdate = new DateTime();
 <div class="stellarnav dark desktop"><a href="https://newssitedesign.com/newsflash/#" class="menu-toggle full"><span class="bars"><span></span><span></span><span></span></span> </a><ul id="menu-main-menu" class="menu"><li id="menu-item-89" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-89"><a href="https://newssitedesign.com/newsflash" aria-current="page"> <i class="fa-solid fa-house-user"></i>  HOME</a></li>
 
 @php
-$categories = App\Models\Category::orderBy('category_name','ASC')->get();
+    $categories = App\Models\Category::orderBy('category_name','ASC')->get();
 @endphp
 
 @foreach($categories as $category)
-<li id="menu-item-291" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-291"><a href=" ">{{ $category->category_name }}</a>
+<li id="menu-item-291" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-291">
+    <a href=" ">{{ $category->category_name }}</a>
 
-@php
-$subcategories = App\Models\Subcategory::where('category_id', $category->id)->orderBy('subcategory_name','ASC')->get();
-@endphp    
-    <ul class="sub-menu">
-    @foreach($subcategories as $subcategory)
-        <li id="menu-item-294" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-294"><a href=" ">{{$subcategory->subcategory_name}}</a>
-        </li>
-    @endforeach
-    </ul>
-<a class="dd-toggle" href=" "><span class="icon-plus"></span></a></li>
+        @php
+            $subcategories = App\Models\Subcategory::where('category_id', $category->id)->orderBy('subcategory_name','ASC')->get();
+        @endphp    
+        <ul class="sub-menu">
+            @foreach($subcategories as $subcategory)
+                <li id="menu-item-294" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-294"><a href=" ">{{$subcategory->subcategory_name}}</a>
+                </li>
+            @endforeach
+        </ul>
+    <a class="dd-toggle" href=" "><span class="icon-plus"></span></a>
+</li>
 @endforeach
-
-
-
-<li id="menu-item-277" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-277"><a href=" ">ARCRIVE</a></li>
 
 </ul>
 <a class="dd-toggle" href=" "><span class="icon-plus"></span></a></li>
