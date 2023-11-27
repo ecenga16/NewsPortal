0aeead33da4418a6a0a2a2e29808f0eb5048ae46
aldocenga <eraldocenga@nmc.al>
2023-11-27 17:01:45 +0100

    Create 404 Page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('frontend.home_dashboard')
+
+@section('title') 
+    404 Page Not Found 
+@endsection
+
+@section('home')
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>404 Page Is Not Found</h1>
+            </div>
+        </div>
+    </div>
+@endsection
