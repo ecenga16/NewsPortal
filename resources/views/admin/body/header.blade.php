176448302aeca845c01afde905a9ebf8d2db66ae
aldocenga <eraldocenga@nmc.al>
2023-12-04 10:31:46 +0100

    Admin image removed from header

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -30,7 +30,6 @@
 
             <li class="dropdown notification-list topbar-dropdown">
                 <a class="nav-link dropdown-toggle nav-user me-0 waves-effect waves-light" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                    <img src="{{asset('backend/assets/images/users/user-1.jpg')}}" alt="user-image" class="rounded-circle">
                     <span class="pro-user-name ms-1">
                         ADMIN <i class="mdi mdi-chevron-down"></i> 
                     </span>
